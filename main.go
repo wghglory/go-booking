9cb69b06978f5fff3590bc55bd2db81fa462dad8
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-02 19:56:27 +0800

    feat: println

--- main.go
+++ main.go
@@ -4,5 +4,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = conferenceTickets
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total", conferenceTickets, "tickets", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
 }
