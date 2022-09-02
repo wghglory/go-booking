0006219a10b96dc60fd5e4861302a48f813d9f23
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-02 20:21:07 +0800

    feat: %T

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total %v tickets, and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	// %T: type
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+
+	// %v: value
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total %v tickets, and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+
+	userName = "Derek"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
