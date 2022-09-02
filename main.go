b545a025348e49a4ccb88bcdfefd84ef9d05dcc7
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-02 20:11:18 +0800

    feat: Printf

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total", conferenceTickets, "tickets", remainingTickets, "are still available")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total %v tickets, and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
